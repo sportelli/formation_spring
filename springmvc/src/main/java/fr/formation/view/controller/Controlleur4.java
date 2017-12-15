23a39e8059bbbe5f24db28bd730714f6eae88f3e
Felix <sportelli@gmail.com>
2017-12-15 13:54:22 +0100

    Gestion de la session HTTP

---
+++ springmvc/src/main/java/fr/formation/view/controller/Controlleur4.java
@@ -0,0 +1,26 @@
+package fr.formation.view.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Controlleur4 {
+	// Récupération de la session HTTP
+	@Autowired
+	private HttpSession session;
+		
+	@GetMapping("/page5.htm")
+	public String page5(Model model) {
+		Integer nbVisites = 0;
+		if (this.session.getAttribute("nbVisites") != null)
+			nbVisites = (Integer) this.session.getAttribute("nbVisites");
+		model.addAttribute("nbVisites", ++nbVisites);
+		this.session.setAttribute("nbVisites", nbVisites);
+		return "page5";
+	}
+	
+}
